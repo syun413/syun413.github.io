f8121c8f15ad0f31747fe31df3f3adb5613b70a0
Syun <b10505044@ntu.edu.tw>
2024-02-25 21:37:28 +0800

    chirpy theme

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708867141';
+const cacheName = 'chirpy-1708868204';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
