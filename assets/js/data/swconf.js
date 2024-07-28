2bc72d01dd3193b27848f4d51ba0e978d188e400
Syun <b10505044@ntu.edu.tw>
2024-07-28 21:23:32 +0800

    Test Script

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722172777',resources: [
+    cacheName: 'chirpy-1722173012',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
