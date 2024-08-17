ad80cd5dba17a887dd08aafc91764a383829d810
Syun <b10505044@ntu.edu.tw>
2024-08-17 23:32:22 +0800

    Add workstation-docker

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723907497',resources: [
+    cacheName: 'chirpy-1723908742',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
