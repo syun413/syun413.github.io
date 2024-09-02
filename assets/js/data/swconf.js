665d4524a3c5d56028ee442a04f2967dd12a319e
Syun <b10505044@ntu.edu.tw>
2024-09-02 18:58:03 +0800

    Fix workstation Machine Learning Tutorial

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725274613',resources: [
+    cacheName: 'chirpy-1725274683',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
